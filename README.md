0f9b451e3d4b09283f541b3092e934b8368cf29d
Ivan Nikitin <ivan@nikitin.biz>
2022-12-25 18:32:25 +0300

    Начальная инициализация

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# conditional-shipping-for-woocommerce-rest-api
-Расширение плагина conditional-shipping-for-woocommerce методами REST API
+Conditional Shipping For WooCommerce REST API
+=============================================
+Версия 1.0.0
+
+Расширение плагина conditional-shipping-for-woocommerce методами REST API.
+
+Необходимо для передачи правил доставки в приложения, использующие REST API.
---
+++ conditional-shipping-for-woocommerce-rest-api.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Conditional Shipping For WooCommerce REST API
+ *
+ * @package     conditional-shipping-for-woocommerce-rest-api
+ * @author      Иван Никитин
+ * @copyright   2022 IvanNikitin.com
+ * @license     GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name: Conditional Shipping For WooCommerce REST API
+ * Plugin URI:  https://github.com/ivannikitin-com/conditional-shipping-for-woocommerce-rest-api
+ * Description: Плагин добавляет функции REST API к плагину Conditional Shipping For WooCommerce
+ * Version:     1.0.0
+ * Author:      Иван Никитин
+ * Author URI:  https://ivannikitin.com
+ * Text Domain: conditional-shipping-for-woocommerce-rest-api
+ * License:     GPL v2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+defined( 'ABSPATH' ) or die( 'Вы рыбов продаёте?' );
+
