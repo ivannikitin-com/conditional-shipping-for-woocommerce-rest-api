a6e778d7e1630f729596f9b7a19d506999ae8c07
Ivan Nikitin <ivan@nikitin.biz>
2022-12-28 19:50:57 +0300

    Отладка кода

--- README.md
+++ README.md
@@ -2,6 +2,16 @@ Conditional Shipping For WooCommerce REST API
 =============================================
 Версия 1.0.0
 
-Расширение плагина conditional-shipping-for-woocommerce методами REST API.
+Расширение плагина [conditional-shipping-for-woocommerce](https://ru.wordpress.org/plugins/conditional-shipping-for-woocommerce/) методами REST API.
 
 Необходимо для передачи правил доставки в приложения, использующие REST API.
+
+Конечная точка
+--------------
+Конечной точкой является `/wp/v3/wcs_ruleset`.  
+Пример URL: `https://example.com/wp-json/wp/v3/wcs_ruleset`
+
+Авторизация пользователя
+------------------------
+Авторизация пользователя выполняется через пароли приложений WP и REST API ядра WP, не через ключи REST API WC!!!
+Запрос возможен для пользователей с правами `read_private_posts`.
--- conditional-shipping-for-woocommerce-rest-api.php
+++ conditional-shipping-for-woocommerce-rest-api.php
@@ -20,3 +20,72 @@
  */
 defined( 'ABSPATH' ) or die( 'Вы рыбов продаёте?' );
 
+/**
+ * Инициализация REST API
+ */
+add_action( 'rest_api_init', function(){
+    // Добавляем маршрут 
+    register_rest_route( 'wp/v3', '/wcs_ruleset', array(
+        'methods'   => 'GET',
+        'callback'  => 'get_conditional_shipping_for_woocommerce_rest',
+        'args'      => array()
+    ) );    
+} );
+
+/**
+ * Функция возвращает записи wcs_ruleset
+ * @param WP_REST_Request $request  Объект запроса
+ * @return WP_REST_Response         Объект ответа
+ */
+function get_conditional_shipping_for_woocommerce_rest( $request ){
+    // Авторизован ли пользователь?
+    if ( ! is_user_logged_in() ){
+        return new WP_Error( 'Unauthorized', 'You\'re unauthorized!', array( 'status' => 401 ) );
+    }
+
+    // Права пользователя
+    if( ! current_user_can('read_private_posts' ) ){
+        return new WP_Error( 'Forbidden', 'Request Forbidden!', array( 'status' => 403 ) );
+    }
+
+    // Запрос всех правил доставки
+     $posts = get_posts( array(
+        'post_type'     => 'wcs_ruleset',
+        'post_status'   => 'publish',
+        'nopaging'      => true,
+        'orderby'       => 'title',
+        'order'         => 'ASC'
+    ) );
+
+    // Формируем результат
+    $result = array();
+    foreach( $posts as $post ){
+        $ruleset = array(
+            'ID' => $post->ID,
+            'title' => $post->post_title 
+        );
+
+        // Мета-данные CPT
+        foreach ( get_metadata( 'post', $post->ID ) as $meta_name => $meta_value ){
+            // Имя свойства
+            $property = str_replace( '_wcs_', '',  $meta_name );
+
+            // Значение свойства
+            $value = is_array( $meta_value ) ? $meta_value[0]: $meta_value;
+            switch ( $meta_name ) {
+                case '_wcs_actions':
+                case '_wcs_conditions':
+                    $ruleset[ $property ] = unserialize( $value );
+                    break;
+
+                default:
+                    $ruleset[ $property ] = $value;
+            }
+        }
+
+        $result[] = $ruleset;
+    }
+
+    // Возвращаем результат
+    return new WP_REST_Response( $result );
+}
